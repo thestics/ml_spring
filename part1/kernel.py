69624be0034315d69f6a31d5da8c958df4ac74a7
Danil Kovalenko <danil.kovalenko@anvileight.com>
2021-05-13 21:50:47 +0300

    Init commit

---
+++ part1/kernel.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+def build_kernel(X, Y, kernel_func):
+    n = X.shape[0]
+    m = Y.shape[0]
+    K = np.zeros((n, m))
+
+    for i in range(n):
+        for j in range(m):
+            x = X[i, :]
+            y = Y[j, :]
+            K[i][j] = kernel_func(x, y)
+
+    return K
+
+
+def polynomial_kernel(X, Y, c, p):
+    """
+        Compute the polynomial kernel between two matrices X and Y::
+            K(x, y) = (<x, y> + c)^p
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            c - a coefficient to trade off high-order and low-order terms (scalar)
+            p - the degree of the polynomial kernel
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+
+    def kernel_func(x, y):
+        return (c + x.dot(y)) ** p
+
+    return build_kernel(X, Y, kernel_func)
+
+
+
+def rbf_kernel(X, Y, gamma):
+    """
+        Compute the Gaussian RBF kernel between two matrices X and Y::
+            K(x, y) = exp(-gamma ||x-y||^2)
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            gamma - the gamma parameter of gaussian function (scalar)
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+
+    def kernel_func(x, y):
+        return np.exp(-gamma * np.linalg.norm(x - y) ** 2)
+
+    return build_kernel(X, Y, kernel_func)
+
